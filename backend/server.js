6a0ebfe7a6e0cbf0a687708d1a5d8230faa0c88e
MohdAseel <mohd.aseel.vkd@gmail.com>
2024-11-08 15:02:54 +0530

    working patch

--- backend/server.js
+++ backend/server.js
@@ -15,31 +15,6 @@ app.use(cors());
 
 app.use("/", Router);
 
-// Handle POST request to add events to the database
-app.post("/:clubId", async (req, res) => {
-  try {
-    const { clubId, eventId, title, startdate, enddate, location, description, image } = req.body;
-
-    const event = new EventModal({
-      clubId,
-      eventId,
-      title,
-      startdate,
-      enddate,
-      location,
-      description,
-      image,
-    });
-
-    await event.save();
-    res.status(200).send("Event saved successfully");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error saving event");
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,120 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './components/components.css';
-import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CreateEvent from './components/CreateEvent';
-import EventCard from './components/EventCard';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EventCard from "./Components/EventCard.jsx";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import "./App.css";
+import LoginPage from "./Pages/LoginPage.jsx";
+import Sports from "./Pages/Sports.jsx";
+import Technical from "./Pages/Technical.jsx";
+import Cultural from "./Pages/Cultural.jsx";
+import Acads from "./Pages/Acads.jsx";
+import WeekView from "./Pages/WeekView.jsx";
+import DayView from "./Pages/DayView.jsx";
+import ERROR404 from "./Pages/ERROR404.jsx";
+import MonthView from "./Pages/MonthView.jsx";
+import ClubPage from "./Components/Clubpage.jsx";
+import Pagedata from "./Pages/pagedata/PageData.jsx";
+import DataProvider from "./context/DataProvider.jsx";
 
-export default App;
+export default function App() {
+  const [isAuthenticated, isUserAuthenticated] = useState(false);
 
-// client/src/index.js
+  const PrivateRoute = ({ isUserAuthenticated, ...props }) => {
+    return isAuthenticated ? (
+      <>
+        <Outlet />
+      </>
+    ) : (
+      <Navigate replace to="/login" />
+    );
+  };
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+  return (
+    <DataProvider>
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dayview" element={<DayView />} />
+          <Route path="/weekview" element={<WeekView />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/technical" element={<Technical />} />
+          <Route path="/acads" element={<Acads />} />
+          <Route path="/cultural" element={<Cultural />} />
+          <Route path="/monthview" element={<MonthView />} />
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+          <Route path="/:clubId" element={<ClubPage />} />
+          <Route path="*" element={<ERROR404 />} />
+        </Routes>
+      </Router> */}
+      <Router>
+        <Routes>
+          <Route
+            path="/login"
+            element={<LoginPage isUserAuthenticated={isUserAuthenticated} />}
+          ></Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/dayview" element={<DayView />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/weekview" element={<WeekView />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/sports" element={<Sports />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/technical" element={<Technical />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/acads" element={<Acads />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/cultural" element={<Cultural />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/monthview" element={<MonthView />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/:clubId" element={<ClubPage />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/*" element={<ERROR404 />} />
+          </Route>
+        </Routes>
+      </Router>
+    </DataProvider>
+  );
+}
